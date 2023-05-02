2fe3a07c7e20e88db4c98e46e080ae780d99ed25
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-05-02 14:27:33 -0700

    Created functions

---
+++ Functions/simple_functions.js
@@ -0,0 +1,19 @@
+//Exercise 1 page 10
+
+function favoriteThings(){
+    console.log("Kaye Poblete");
+    console.log("The Silence of the Lambs");
+    console.log("Queen");
+}
+
+function whyImHere(){
+    console.log("I joined because I wanted to learn more about programming and get a job in tech.");
+}
+
+function favoritePlace(){
+    console.log("One of my favorite places to visit is Tabaco City in the Philippines because that's where my mom and some family live so it's always fun to visit.");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
