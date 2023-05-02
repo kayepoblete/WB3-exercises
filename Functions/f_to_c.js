67aa2d32923ab864bf88382732ba50648fbac224
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-05-02 15:33:48 -0700

    Created f_to_c.js
    Created f_to_c.js

---
+++ Functions/f_to_c.js
@@ -0,0 +1,15 @@
+//Exercise 1 page 21
+
+function convertFtoC(fahrenheitTemp) {
+    return (fahrenheitTemp - 32)*(5/9);
+}
+
+let currentTemp = 92;
+let celsiusTemp = convertFtoC(currentTemp);
+// console.log(celsiusTemp);
+console.log(convertFtoC(212));
+console.log(convertFtoC(90));
+console.log(convertFtoC(72));
+console.log(convertFtoC(32));
+console.log(convertFtoC(0));
+console.log(convertFtoC(-40));
