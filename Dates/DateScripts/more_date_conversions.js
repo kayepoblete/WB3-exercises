2adb06c2d19f82c287d2213f12c3f1ced20a8546
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-05-03 13:55:29 -0700

    Created more_date_conversions.js
    Created more_date_conversions.js

---
+++ Dates/DateScripts/more_date_conversions.js
@@ -0,0 +1,11 @@
+// Exercise 2 page 68
+"use strict";
+
+let myBirthday = new Date("04/26/1993");
+console.log(myBirthday.toLocaleString());
+
+let momBirthday = new Date("June 13 1966");
+console.log(momBirthday.toLocaleString());
+
+let dadBirthday = new Date("1966, 10, 6");
+console.log(dadBirthday.toLocaleString());
