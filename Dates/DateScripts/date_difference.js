935c6d73192b7b8adc25f00f46b5b42598f9d4a4
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-05-03 14:12:38 -0700

    Created date_difference.js
    Created date_difference.js

---
+++ Dates/DateScripts/date_difference.js
@@ -0,0 +1,14 @@
+// Exercise 1 page 71
+"use strict";
+
+let startDate = new Date("July 11, 2022");
+let endDate = new Date("November 11, 2022");
+
+let msec_per_day = 1000 * 60 * 60 * 24;
+
+let elapsedMilliseconds = endDate.getTime() - startDate.getTime();
+
+let dayDiff = elapsedMilliseconds / msec_per_day;
+let numDays = Math.round(dayDiff);
+
+console.log(`The number of days between date is ${numDays}`);
